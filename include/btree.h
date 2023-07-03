46bac323ead7e1ceed6e8c0c121723b88aad7b19
Nicola Vermeulen <nicola@lolwut.co.za>
2023-07-03 15:45:16 +0200

    Add header file for btree

---
+++ include/btree.h
@@ -0,0 +1,46 @@
+#ifndef BTREE_H
+
+#define BTREE_H
+
+#include <stdio.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <optional>
+using namespace std;
+
+const int DEGREE = 64;
+
+class Node {
+ public:
+  int *keys;
+  Node **children;
+  bool IS_LEAF;
+  int numElements;
+
+  Node();
+
+  friend class BTree;
+};
+
+class Btree {
+ public:
+  Node *root;
+
+  Btree();
+  void insertInternal(int, Node *, Node *);
+
+  void insert(int n);
+  Node *findParent(Node *, Node *);
+  int search(int value);
+
+  void printTree(Node *node);
+};
+
+inline Node::Node() {
+  keys = new int[DEGREE];
+  children = new Node *[DEGREE + 1];
+}
+
+inline Btree::Btree() { root = NULL; }
+#endif
